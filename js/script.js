aaf0f5205f2fe7604162474f9a68d0d9ad0eae9a
Yura-by <s.older681@gmail.com>
2019-03-04 19:23:43 +0300

    javaEsc

--- js/script.js
+++ js/script.js
@@ -13,10 +13,14 @@ var write = document.querySelector(".write-us");
 writeOpen.addEventListener("click", function (evt) {
 	evt.preventDefault();
 	write.classList.add("write-us--show");
-	fullName.focus();
+	
 	if (storage) {
 		fullName.value = storage;
-	}
+		write-us-email.focus();
+		} else {
+			write-us-login.focus();
+		}
+
 });
 
 var closeMap = document.querySelector(".location-map__close");
@@ -39,12 +43,20 @@ var forma = document.querySelector(".write-us-form");
 forma.addEventListener("submit", function (evt) {
 	if (!fullName.value || !eMail.value || !text.value) {
 	evt.preventDefault();
-console.log('Не ввелииии');
 } else {
 	localStorage.setItem("fullName", fullName.value);
 }
 });
 
 var storage = localStorage.getItem("fullName");
+
+/*window.addEventListener("keydown", function(evt) {
+	if (evt.keyCode === 27) {
+		if (write-us.classList.contains(write-us--show)) {
+			evt.preventDefault();
+			write-us.classList.remove(write-us--show);
+		}
+	}
+});*/
 
 
